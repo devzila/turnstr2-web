506ff72a477798ed7448179e075e436020e83a64
Deepmala <deepmala@devzila.com>
2017-12-19 13:05:04 +0530

    member login

--- app/controllers/member/base_controller.rb
+++ app/controllers/member/base_controller.rb
@@ -1,3 +1,14 @@
 class Member::BaseController < ApplicationController
+  layout 'application'
+  before_filter :authorize
 
+
+  def current_member
+    @current_member ||= User.find(session[:member_id]) if session[:member_id]
+  end
+  helper_method :current_member
+
+  def authorize
+    redirect_to new_member_sessions_path unless current_member
+  end
 end
