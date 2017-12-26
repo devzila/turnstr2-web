b78cf7c0c781ba507f29d191d4bdf13d23623b67
Nilay <nilayanand@gmail.com>
2017-12-27 01:59:36 +0530

    make config change as per production server

--- config/puma.rb
+++ config/puma.rb
@@ -9,11 +9,16 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3001 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+bind  "unix:///tmp/turnstr-web/turnstr-web.sock"
+pidfile "/tmp/turnstr-web/pid"
+state_path "/tmp/turnstr-web/state"
+
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
