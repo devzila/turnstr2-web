46728cdf3ebc688c23868823f8ed960e9c2edbdb
Nilay <nilayanand@gmail.com>
2018-07-05 16:46:45 +0530

    Set video for story share page

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -18,6 +18,14 @@ class StoryController < ApplicationController
     @face5_url = @story.get_face_thumb_image(5) || @face3_url
     @face6_url = @story.get_face_thumb_image(6) || @face4_url
 
+
+    @face1_ori_url = @story.face1_media.url
+    @face2_ori_url = @story.face2_media.url || @face1_ori_url
+    @face3_ori_url = @story.face3_media.url || @face1_ori_url
+    @face4_ori_url = @story.face4_media.url || @face2_ori_url
+    @face5_ori_url = @story.face5_media.url || @face3_ori_url
+    @face6_ori_url = @story.face6_media.url || @face5_ori_url
+
     @user_name = @story.user.username || @story.user.name
 
   end
